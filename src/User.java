6aa93d0cb7524d7da0a28172bc70a16c74499d22
Abdullah Alhadad <lr.aalhadad@mmdc.mcl.edu.ph>
2026-03-03 12:15:15 +0800

    Refactor User class with constructor and login fix
    Fix constructor syntax and correct login method typo.

--- src/User.java
+++ src/User.java
@@ -1,1 +1,26 @@
+public abstract class User {
 
+  private String username;
+  private String password;
+  private String role;
+
+  public User(String username, String password, String role) [
+    this.username = username;
+    this.password = password;
+    this.role = role;
+}
+
+    public boolean login(String username, String password) {
+      return this.usernamne.equals(username) && this.password.equals(password);
+}
+
+  public abstract void accessSystem();
+
+  public String getRole() {
+      return role;
+}
+
+    public void setRole(String role) {
+        this.role = role;
+  }
+}
