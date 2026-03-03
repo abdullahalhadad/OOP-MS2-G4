b077731a60fcd089851a44b302a899e0020fa81a
Abdullah Alhadad <lr.aalhadad@mmdc.mcl.edu.ph>
2026-03-03 12:58:50 +0800

    Implement employee creation and payroll processing

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,43 @@
+public classs Main {
 
+  public static void main(String[] args) {
+
+    // Create regular employee
+    Employee regular = new RegularEmployee(
+            "E001",
+            "Juan",
+            "Dela Cruz",
+            "Staff",
+            30000
+      );
+
+      // Create Contractual Employee
+        Employee contractual = new ContractualEmployee(
+                "E002",
+                "Maria",
+                "Santos",
+                "Contractual",
+                500,      // hourly rate
+                160       // hours worked
+        );
+
+        // Create Payroll Processor
+        PayrollProcessor processor = new PayrollProcessor();
+
+        // Process Regular Employee
+        System.out.println("===== REGULAR EMPLOYEE =====");
+        processor.displayPayrollSummary(regular);
+
+        System.out.println();
+
+        // Process Contractual Employee
+        System.out.println("===== CONTRACTUAL EMPLOYEE =====");
+        processor.displayPayrollSummary(contractual);
+
+        System.out.println();
+
+        // Test method overloading
+        double bonusSalary = processor.calculateSalary(regular, 5000);
+        System.out.println("Regular with Bonus: " + bonusSalary);
+    }
+}
