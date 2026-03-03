d71115b78b2fb0faaa7e54ce135fcdc4a337ad7a
Abdullah Alhadad <lr.aalhadad@mmdc.mcl.edu.ph>
2026-03-03 12:27:10 +0800

    Enhance Employee class with payroll methods
    Refactor Employee class to include payroll calculations and additional fields.

--- src/Employee.java
+++ src/Employee.java
@@ -1,10 +1,15 @@
 public abstract class Employee {
 
-    protected String employeeId;
-    protected String firstName;
-    protected String lastName;
-    protected String position;
+    //Identity
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String position;
+
+    // Payroll fields
     protected double basicSalary;
+    protected double hourlyRate;
+    protected double hoursWorked;
 
     public Employee(String employeeId, String firstName, String lastName,
                     String position, double basicSalary) {
@@ -15,25 +20,69 @@ public abstract class Employee {
         this.basicSalary = basicSalary;
     }
 
-    public Employee() {}
+    // Abstract method (polymorphism)
+    public abstract double calculateGrossPay();
 
-    // Abstract behavior (must be implemented by child classes)
-    public abstract String getRole();
+    // shared payroll computations
+    public double computeSSS() {
+        return calculateGrossPay() * 0.045; //example rate
+    }
+
+    public double computePhilHealth() {
+        return calculateGrossPay() * 0.02;
+    }
+
+    public double computePagIBIG() {
+        return 100; //fixed sample
+    }
+
+    public double computeTax() {
+        return calculateGrossPay() * 0.10;
+    }
+
+    public double computeNetPay() {
+        return calculateGrossPay()
+                - computeSSS()
+                - computePhilHealth()
+                - computePagIbig()
+                - computeTax();
+    }
+
+    // Getters & Setters
+    public String getEmployeeId() { return employeeId; }
+    public void setEmployeeeId(String employeeId) { this.employeeId = employeeId; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+     public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public double getBasicSalary() { return basicSalary; }
+    public void setBasicSalary(double basicSalary) {
+        if (basicSalary > 0) {
+            this.basicSalary = basicSalary;
+        }
+    }
+
+    public double getHourlyRate() { return hourlyRate; }
+    public void setHourlyRate(double hourlyRate) {
+        if (hourlyRate > 0) {
+            this.hourlyRate = hourlyRate;
+        }
+    }
+
+    public double getHoursWorked() { return hoursWorked; }
+    public void setHoursWorked(double hoursWorked) {
+        if (hoursWorked >= 0) {
+            this.hoursWorked = hoursWorked;
+        }
+    }
 
     @Override
     public String toString() {
-        return employeeId + " - " + firstName + " " + lastName + " (" + position + ")";
+        return employeeId + " - " + firstName + " " + lastName;
     }
 }
-
-public String getEmployeeId() { return employeeId; }
-public void setEmployeeId(String employeeId) { this.employeeId = employeeId; }
-
-public String getFirstName() { return firstName; }
-public void setFirstName(String firstName) { this.firstName = firstName; }
-
-public String getLastName() { return lastName; }
-public void setLastName(String lastName) { this.lastName = lastName; }
-
-public double getBasicSalary() { return basicSalary; }
-public void setBasicSalary(double basicSalary) { this.basicSalary = basicSalary; }
+    
+ 
