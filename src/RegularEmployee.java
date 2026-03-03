8544882120a31bd6c5ea0df448665353b6e9c568
Abdullah Alhadad <lr.aalhadad@mmdc.mcl.edu.ph>
2026-03-03 12:29:29 +0800

    Implement RegularEmployee class extending Employee

--- src/RegularEmployee.java
+++ src/RegularEmployee.java
@@ -1,1 +1,12 @@
+public class RegularEmployee extends Employee {
 
+  public RegularEmployee(String employeeId, String firstName, String lastName,
+                         String position, double basicSalary) {
+    super(employeeId,, firstName, lastName, position, basicSalary);
+  }
+
+  @Override
+  public double calculateGrossPay() {
+    return basicSalary;
+  }
+}
